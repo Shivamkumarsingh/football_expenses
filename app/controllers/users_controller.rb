6e0cde330a86e1bc84a3b45d80b011738cb8564b
Anil <anilmaurya8dec@gmail.com>
2015-08-13 13:28:57 +0530

    List user's payment list in desc game's date order
    Optimize game and payment load query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def games
-    @payments = @user.payments
+    @payments = Payment.where(user_id: @user.id).includes(:game).order('games.date desc')
   end
 
   private
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   validates :date, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
 
-  has_many :payments
+  has_many :payments, dependent: :destroy
 
   accepts_nested_attributes_for :payments, allow_destroy: true
 end
